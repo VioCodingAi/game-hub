10b6aaf3b3eaef1223524b11b8ab883e40e426e6
VioCodingAi <vivoviva@gmail.com>
2025-02-16 23:28:52 +0800

    Refactor GameGrid to use SimpleGrid for layout; add GameCard component for individual game display; export Game interface with background_image property

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
 
   return (
-    <ul>
+    <>
       {error && <Text>{error}</Text>}
-      {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
-      ))}
-    </ul>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+        {games.map((game) => (
+          <GameCard game={game} key={game.id} />
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
 
