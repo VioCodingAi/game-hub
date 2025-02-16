457000bc65b511c59c41def66822a5b6aa024153
VioCodingAi <vivoviva@gmail.com>
2025-02-16 21:58:47 +0800

    Add ColorModeSwitch component and update NavBar layout; refine App component structure

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,19 @@
+import { HStack, Switch, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      >
+        Dark Mode
+      </Switch>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
