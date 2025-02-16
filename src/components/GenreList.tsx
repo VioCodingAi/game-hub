cd7867f39a25cb656f95e9ef2ab033fab6634886
VioCodingAi <vivoviva@gmail.com>
2025-02-17 01:49:06 +0800

    Add GenreList component and useGenres hook to display game genres in the sidebar

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,21 @@
+import { Spinner } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres, error, isLoading } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
+  return (
+    <>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GenreList;
