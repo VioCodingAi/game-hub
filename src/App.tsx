54e57f289869d00e3bd84c4e4200f49c99e5a77d
VioCodingAi <vivoviva@gmail.com>
2025-02-17 10:51:20 +0800

    Add search functionality to NavBar and integrate with game query

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -31,7 +32,11 @@ function App() {
         paddingX={2}
       >
         <GridItem area="nav">
-          <NavBar />
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
+          />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" paddingX={2}>
