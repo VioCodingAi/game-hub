749fe7fa5fbc113c8605c860c7645bdcd72c2a8d
VioCodingAi <vivoviva@gmail.com>
2025-02-18 15:20:47 +0800

    Clean up

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryFn: () =>
       apiClient
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
-        .then((res) => res.data), // Corrected endpoint
+        .then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: platforms.length, results: platforms },
   });
