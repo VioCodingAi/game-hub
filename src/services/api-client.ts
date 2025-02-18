c0baf1b4fb943e57f77fb6bc75b873b729c81931
VioCodingAi <vivoviva@gmail.com>
2025-02-19 02:08:30 +0800

    Refactor API client to use a unified getAll method and update hooks to improve data fetching

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosinstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "2ac56f09840e4d2ba8f1b842425ce58b",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosinstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
